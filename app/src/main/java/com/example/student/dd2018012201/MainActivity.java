fdd490069ff819c480bbb1d5588f46059c542df7
kcli2412 <kcli2412@gmail.com>
2018-01-22 10:23:22 +0800

    OutputStream SQLiteDB

--- app/src/main/java/com/example/student/dd2018012201/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018012201/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.student.dd2018012201;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        File dbFile = new File(getFilesDir(), "student.db");
+        InputStream is = getResources().openRawResource(R.raw.student);
+        try {
+            OutputStream os = new FileOutputStream(dbFile);
+            int r;
+            while ((r = is.read()) != -1)
+            {
+                os.write(r);
+            }
+            is.close();
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
